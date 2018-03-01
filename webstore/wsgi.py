5d17ac9341bc4e9cd7b21437354bd15f2a17f91c
Hannu Junno <hannu.junno@gmail.com>
2018-03-01 15:27:35 +0200

    Removed django-dotenv package since .env can be loaded with pipenv

--- webstore/wsgi.py
+++ webstore/wsgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-import dotenv
-from django.core.wsgi import get_wsgi_application
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webstore.settings")
 
