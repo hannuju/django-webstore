608e88bf6b0aa0da802925123d23ea27df9ba55d
Hannu Junno <hannu.junno@gmail.com>
2018-03-03 16:51:31 +0200

    Added search bar

--- store/views.py
+++ store/views.py
@@ -1,12 +1,19 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-#from store.forms import SearchForm
+from django.db.models import Q
 from store.models import Item
 
 class IndexView(ListView):
     model = Item
     template_name = 'store/index.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Item.objects.filter(Q(id__icontains=query) | Q(title__icontains=query))
+        else:
+            return Item.objects.all()
 
 class DetailView(DetailView):
     model = Item
