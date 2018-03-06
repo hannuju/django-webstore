e9a243975acb26a612e870e0336e4874e23bf77b
Hannu Junno <hannu.junno@gmail.com>
2018-03-06 16:28:18 +0200

    Cleaned up code

--- store/views.py
+++ store/views.py
@@ -13,10 +13,9 @@ class IndexView(ListView):
     template_name = 'store/index.html'
 
     def get_queryset(self):
-        # Save the session key first time opening the index page
+        # First time opening the index page save the session to create a session key
         if not self.request.session.session_key:
             self.request.session.save()
-
         # Search with SQL-query "Like"
         query = self.request.GET.get('q')
         if query:
@@ -41,26 +40,21 @@ class ItemDelete(DeleteView):
     success_url = reverse_lazy('item-list')
 
 def CartView(request):
-    # Add cart to context if it exists
     key = request.session.session_key
-    print(key)
+    context = {}
+    # Add users cart to context if it exists
     if not cache.get(key) is None:
-        print("[INFO] Cart found!")
         context = {'obj' : cache.get(key)}
-    else:
-        print("[INFO] Cart NOT found!")
-        context = {}
     return render(request, 'store/cart.html', context = context)
 
 # AJAX call
 def addToCart(request, item_id):
     key = request.session.session_key
-    print(key)
     item = get_object_or_404(Item, pk=item_id)
     # Create cart if it doesn't exist, then add item to cart
     if cache.get(key) is None:
         cache.set(key, Cart())
     userCart = cache.get(key)
     userCart.add_item(item)
-    cache.set(key, userCart, 600) # expire timer = 10 min
+    cache.set(key, userCart, 600) # Expire timer = 10 min
     return HttpResponse("Yeees!")
